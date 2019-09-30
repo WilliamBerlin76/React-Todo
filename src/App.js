046ece6c617317bfc9ba247a4767be3ab0e653de
WilliamBerlin76 <william.berlin76@gmail.com>
2019-09-30 14:47:46 -0500

    adding components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import React from 'react';
+import TodoList from './components/TodoComponents/TodoList'
 
+const todoData = [
+  {
+    task: 'Eat Breakfast',
+    id: 1,
+    completed: false
+  },
+  {
+    task: 'Clean Room',
+    id: 2,
+    completed: false
+  }
+];
 class App extends React.Component {
   // you will need a place to store your state in this component.
+  constructor() {
+    super();
+    this.state = {
+       task: 'task',
+       name: todoData 
+    }
+  }
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <TodoList/>
       </div>
     );
   }
