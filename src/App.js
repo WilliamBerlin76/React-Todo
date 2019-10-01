06403e72c2a621dfffd0e5b8892760d87736a2c2
WilliamBerlin76 <william.berlin76@gmail.com>
2019-09-30 19:27:49 -0500

    no longer any default tasks

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import TodoList from './components/TodoComponents/TodoList';
 import TodoForm from './components/TodoComponents/TodoForm';
-import './components/TodoComponents/Todo.css'
+import './components/TodoComponents/Todo.css';
 
 const todoData = [
   {
-    task: 'Eat Breakfast',
-    id: 1,
+    task: '',
+    id: null,
     completed: false
   },
-  {
-    task: 'Clean Room',
-    id: 2,
-    completed: false
-  }
+  // {
+  //   task: 'Clean Room',
+  //   id: 2,
+  //   completed: false
+  // }
 ];
 class App extends React.Component {
   // you will need a place to store your state in this component.
